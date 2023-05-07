8c43d3fd7e60261a6e89d0d8034a036e35675969
nicolozibra1 <nico.zibra@gmail.com>
2023-05-07 19:47:10 +0200

    added-logout & include-partials-template

--- index.php
+++ index.php
@@ -5,23 +5,14 @@
     $password = getPassword();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>PHP Strong Password Generator</title>
-</head>
-<body>
+<?php include './partials/template/header.php' ?>
     <main>
         <form action="passGen.php" method="GET">
             <label for="pass-length">Enter the desired length, between 8 and 32 characters</label>
             <input type="number" name="pass-length" id="pass-length" min="8" max="32">
             <button type="submit">Generate</button>
         </form>
+        <a href="logout.php">Logout</a>
     </main>
 </body>
 </html>
